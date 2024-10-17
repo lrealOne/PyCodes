02657b3c3455dc11886c2fef1e1827bf9749bd28
Luan Leite <luanleite202@gmail.com>
2024-10-17 15:19:57 -0300

    Pycodes venv virtual ambient

---
+++ lessons/venv.py
@@ -0,0 +1,12 @@
+'''
+Ambientes virtuais Py
+
+Um ambiente virtual carrega toda sua instalação do py para uma pasta no caminho escolhido. 
+
+Ao ativar o ambiente virtual, a instalação do ambiente será usada.
+
+VENV é o modulo que usaremos para criar ambientes virtuais. 
+
+Os nomes mais utilizados pro AV são:
+venv, env, .venv, .env
+'''
